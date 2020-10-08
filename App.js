0ee2ff6eab22d8bb05b8353c642c5d589da71c6f
Dom Youmans <ddyoumans@icloud.com>
2020-10-08 07:19:24 -0700

    add text, photo, and safe area view.

--- App.js
+++ App.js
@@ -1,21 +1,38 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import {
+  StyleSheet,
+  Text,
+  Image,
+  SafeAreaView,
+  TouchableWithoutFeedback,
+} from "react-native";
 
 export default function App() {
+  const handlePress = () => console.log("Text Pressed!");
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <Text>Money Heist</Text>
+      <TouchableWithoutFeedback>
+        <Image
+          source={{
+            width: 200,
+            height: 300,
+            uri: "https://picsum.photos/200/300",
+          }}
+        ></Image>
+      </TouchableWithoutFeedback>
+      <StatusBar style="inverted" />
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    justifyContent: "center",
+    alignItems: "center",
   },
 });
